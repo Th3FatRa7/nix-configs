02871baf5162035818a6560a0fc9a4a024ed694a
th3fatra7 <142910646+Th3FatRa7@users.noreply.github.com>
2026-08-17 05:07:17 -0400

    add: hp-laptop imports

--- hosts/hp-laptop/default.nix
+++ hosts/hp-laptop/default.nix
@@ -1,2 +1,11 @@
-{
+{inputs, ...}: {
+  imports = [
+    inputs.disko.nixosModules.disko
+    ../../modules/nixos/disko.nix
+    ../../modules/nixos/bootloader.nix
+    ../../modules/nixos/common.nix
+    ../../modules/nixos/security.nix
+    ../../modules/nixos/virtualisation.nix
+    ../../modules/nixos/desktop.nix
+  ];
 }
