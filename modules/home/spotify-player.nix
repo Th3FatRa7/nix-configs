e94339bb71563e87981a3f73e05cdfd545b9eb29
th3fatra7 <142910646+Th3FatRa7@users.noreply.github.com>
2026-08-17 16:48:20 -0400

    add: spotify-player relative line numbering for even faster song picking

--- modules/home/spotify-player.nix
+++ modules/home/spotify-player.nix
@@ -3,10 +3,15 @@
     enable = true;
     settings = {
       enable_notify = false;
+
       liked_icon = "";
       explicit_icon = "󰬌";
+
       border_type = "Thick";
+
       enable_mouse_scroll_volume = false;
+
+      enable_relative_line_number = true;
     };
   };
 
