5b5772214eeced00e28b38d7abc04f654c2c72cb
th3fatra7 <142910646+Th3FatRa7@users.noreply.github.com>
2026-08-17 05:06:48 -0400

    add: plymouth configuration and custom theme

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,3 +1,28 @@
-{
-  boot.loader.systemd-boot.enable = true;
+{pkgs, ...}: {
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.timeout = 0;
+
+    initrd.verbose = false;
+    initrd.kernelModules = ["i915"];
+
+    consoleLogLevel = 3;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=false"
+      "systemd.show_status=false"
+    ];
+
+    plymouth = {
+      enable = true;
+      themePackages = with pkgs; [
+        adi1090x-plymouth-themes
+      ];
+      theme = "hexagon_dots_alt";
+    };
+  };
 }
