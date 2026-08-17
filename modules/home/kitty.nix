bf3b26ec9dd0b28ae105eff192d61f3e3ff39891
th3fatra7 <142910646+Th3FatRa7@users.noreply.github.com>
2026-08-17 16:00:15 -0400

    add: more kitty configuration

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -7,11 +7,23 @@
 
     settings = {
       background_opacity = 0.85;
+
       cursor_blink_interval = "0.5 ease-in-out";
       cursor_stop_blinking_after = 0;
       cursor_trail = 1;
       cursor_trail_decay = "0.1 0.2";
       cursor_trail_start_threshold = 1;
+
+      scrollbar = "never";
+      scrollbar_interactive = false;
+
+      sync_to_monitor = true;
+
+      enable_audio_bell = false;
+
+      hide_window_decorations = true;
+
+      confirm_os_window_close = 0;
     };
   };
 }
